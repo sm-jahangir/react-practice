e54f8b15afc1c0cafea4a9f5539ef2ee8dbb863d
sm-jahangir <samraatjahangir@gmail.com>
2022-03-16 15:09:12 +0600

    45-2 JSX, Dynamic content, Dynamic Style in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Dr. Jahangir Alam', job: 'Web Developer'};
+const singerStyle = {
+  backgroundColor: 'yellow',
+  color: 'red',
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={singerStyle}>number {number + 2222}</h1>
+      <h2 style={{color:'red', backgroundColor: 'yellowgreen', padding: '10px 20px'}}>{singer.name} {singer.job}</h2>
     </div>
   );
 }
