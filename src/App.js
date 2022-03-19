723b563ba3dacdeef3278e0a629b05096c14900b
sm-jahangir <samraatjahangir@gmail.com>
2022-03-20 00:01:54 +0600

    45-8 receap completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
     <ExternalUsers></ExternalUsers>
+    <hr />
+    <Counter></Counter>
+    <LoadComment></LoadComment>
     </div>
   );
 }
@@ -30,6 +33,47 @@ function User(props) {
     <div style={{border: '2px solid yellow', margin: '20px', padding: '10px', borderRadius: '10px'}}>
         <h2>{props.name}</h2>
         <p>{props.email}</p>
+    </div>
+  )
+}
+
+// Receap Module
+function Counter() {
+  const [count, setCount] = useState(20);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
+}
+
+
+function LoadComment() {
+  const [comment, setComment] = useState([]);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => response.json())
+    .then(comment => setComment(comment));
+  }, [])
+  return (
+    <div>
+      <h2>Comment Box</h2>
+      {
+        comment.map(comment => <CommentBox name={comment.name} email={comment.email} body={comment.body}></CommentBox>)
+      }
+    </div>
+  )
+}
+function CommentBox(props) {
+  return (
+    <div style={{margin:'20px', backgroundColor: '#ddd', padding: '10px', borderRadius: '20px'}}>
+      <h3>{props.name}</h3>
+      <small style={{backgroundColor: '#08b153', color: 'white', padding: '4px', borderRadius: '21px'}}>{props.email}</small>
+      <p>{props.body}</p>
     </div>
   )
 }
