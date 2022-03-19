e2c2b9d3862963c40cad2a5ed8899d0dd18a0919
sm-jahangir <samraatjahangir@gmail.com>
2022-03-19 22:17:30 +0600

    45-8 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => setUsers(data));
+
+  }, [])
+  return (
+    <div>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+function User(props) {
+  return (
+    <div style={{border: '2px solid yellow', margin: '20px', padding: '10px', borderRadius: '10px'}}>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
+    </div>
+  )
 }
 
 export default App;
