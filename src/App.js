6e104c6f89eddfbff6617a835e5acc063efe2300
sm-jahangir <samraatjahangir@gmail.com>
2022-03-22 23:15:19 +0600

    Post load complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <District name="Kurigram" vibag="Rangpur"></District>
+      <District name="Kurigram" vibag="Rangpur"></District>
+      <District name="Kurigram" vibag="Rangpur"></District>
+      <District name="Kurigram" vibag="Rangpur"></District> */}
+      <LoadPost></LoadPost>
+    
     </div>
   );
 }
+function LoadPost() {
+  const [posts, setPost] = useState([]);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => setPost(posts));
+  }, []);
+  return (
+    <div>
+      <h1>Post:</h1>
+      {
+        posts.map((post, index) => <Post key={index+1} title={post.title}></Post>)
+      }
+    </div>
+  )
+}
+function Post(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+    </div>
+  )
+}
+
+function District(props) {
+  const [power, setPower] = useState(1);
+  const boostPower = () => {
+    const newPower = power * 5;
+    setPower(newPower);
+  }
+  return (
+    <div className='district'>
+      <h2>Division: {props.vibag}</h2>
+      <h3>District: {props.name}</h3>
+      <h4>Power: {power}</h4>
+      <button onClick={boostPower}>Boost The Power</button>
+    </div>
+  )
+}
+
 
 export default App;
