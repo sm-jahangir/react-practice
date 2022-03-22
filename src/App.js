7d01cdeea359053c319b32a05a37988c7a86d352
sm-jahangir <samraatjahangir@gmail.com>
2022-03-22 16:24:21 +0600

    Load all countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect( () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(country => setCountries(country));
+  }, []);
+  return (
+    <div className='container'>
+      <h2 className='text-center my-4 py-4'>Load Countries {countries.length}</h2>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+          {
+            countries.map((country, index) => <Country key={index} name={country.name.common} flags={country.flags.png} population={country.population}></Country>)
+          }
+      </div>
+    </div>
+  )
+}
+function Country(props) {
+  return (
+    <div className="col">
+      <div className="card">
+        <img src={props.flags} style={{width: '200px', height: '100px', margin: 'auto'}} />
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <small>{props.population}</small>
+          <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
