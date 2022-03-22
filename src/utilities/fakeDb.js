978dd9e040cfdcf05c6a9ea2ca5c992da25a2d1c
sm-jahangir <samraatjahangir@gmail.com>
2022-03-23 01:42:57 +0600

    Shopping cart with objects

--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,13 +1,39 @@
+// //use local storage to manage cart data
+// const addToDb = id => {
+//     const quantity = localStorage.getItem(id);
+//     if (quantity) {
+//         const newQuantity = parseInt(quantity) + 1;
+//         localStorage.setItem(id, newQuantity);
+//         console.log('Already Exist');
+//     } else {
+//         localStorage.setItem(id, 1);
+//     }
+// }
+
+// export {addToDb};
+
 //use local storage to manage cart data
 const addToDb = id => {
-    const quantity = localStorage.getItem(id);
-    if (quantity) {
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
-        console.log('Already Exist');
+    let shoppingCart;
+
+    // get the shopping cart
+    const storeCart = localStorage.getItem('shopping-cart');
+    if (storeCart) {
+        shoppingCart = JSON.parse(storeCart);
     } else {
-        localStorage.setItem(id, 1);
+        shoppingCart = {};
     }
+
+    const quantity = shoppingCart[id];
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    } else {
+        shoppingCart[id] = 1;
+    }
+    
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+    
 }
 
 export {addToDb};
