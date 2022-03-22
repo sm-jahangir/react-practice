c88e1189541c5819766116696a443ce187e13f0a
sm-jahangir <samraatjahangir@gmail.com>
2022-03-23 00:25:20 +0600

    Reactjs lifecycle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [steps, setSteps] = useState(0);
+
+  const increaseSteps = () => {
+    const newStepsCount = steps + 1;
+    setSteps(newStepsCount);
+  }
+  useEffect( () => {
+    console.log(steps);
+  }, [steps])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Power: {steps}</h2>
+      <button onClick={increaseSteps}>Click</button>
     </div>
   );
 }
