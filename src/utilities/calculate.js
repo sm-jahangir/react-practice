c95c145f35361180237462ccd54d976782104f6e
sm-jahangir <samraatjahangir@gmail.com>
2022-03-31 12:27:51 +0600

    utilities add and multiply use

---
+++ src/utilities/calculate.js
@@ -0,0 +1,10 @@
+const add = (first, second) => {
+    const groundTotal = first + second
+    console.log(groundTotal);
+}
+const multiply = (first, second) => {
+    const groundTotal = first * second
+    console.log(groundTotal);
+}
+
+export {add, multiply};
