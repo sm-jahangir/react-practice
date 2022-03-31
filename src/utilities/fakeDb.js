37f6ac8c4b73d4216486b35c69f6b25338f09af2
sm-jahangir <samraatjahangir@gmail.com>
2022-03-31 15:57:02 +0600

    localStorage add to cart

--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,12 +1,32 @@
 
 const adToCartDb = (id) => {
-    const quantity = localStorage.getItem(id);
+    let shoppingCart = {};
+    //get the shopping cart
+    const storeCart = localStorage.getItem('shopping-cart');
+    if (storeCart) {
+        // storeCart ta string karon amra atake string hisabe save korchi mane object take amra string hisabe save korchilam
+        shoppingCart = JSON.parse(storeCart);
+        // akhane JSON.parse String data ta object hoye jabe abr ager moto
+    }
+
+
+
+    // JSON.stringify deya hoy karon shoppingCart object take string a convert korar jonno
+    // localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart)); //
+    //oporer 2 line rakha hoiche je kivabe empty object localstorage a save kora jay ata dekhar jonno
+
+    // shoppingCart object er vitore "id" ta ache kina seta check korte hobe
+    const quantity = shoppingCart[id];
     if (quantity) {
         const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+        shoppingCart[id] = newQuantity;
     }else {
-        localStorage.setItem(id, 1);
+        shoppingCart[id] = 1;
     }
+    
+    // JSON.stringify deya hoy karon shoppingCart object take string a convert korar jonno
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+
 }
 const removeToCartDb = (id) => {
     const quantity = localStorage.getItem(id);
