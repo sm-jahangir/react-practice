3a01780cbaf68191046209553709cb512dab42dc
sm-jahangir <samraatjahangir@gmail.com>
2022-03-31 12:14:24 +0600

    button event handler with params

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -2,20 +2,47 @@ import React from 'react'
 import './Product.css'
 
 function Product(props) {
-    const {name, picture, age} = props.product;
-
-    
+    const {id, name, picture, age} = props.product;
+    const addToCart = (id) => {
+      console.log('Items Added', id);
+    }
   return (
     <div className='product'>
         <img src={picture} alt={name} />
         <h2>{name}</h2>
         <p>{age}</p>
-        <button>Add To Card</button>
+        <button onClick={() => addToCart(id)}>Add To Card</button>
     </div>
   )
 }
 
 export default Product
+/*
+import React from 'react'
+import './Product.css'
+
+function Product(props) {
+    const {id, name, picture, age} = props.product;
+
+    const addToCart = (id) => {
+      // console.log('Items Added');
+    }
+    const addToCartWithParams =() => {
+      addToCart(id);
+      console.log(id);
+    }
+  return (
+    <div className='product'>
+        <img src={picture} alt={name} />
+        <h2>{name}</h2>
+        <p>{age}</p>
+        <button onClick={addToCartWithParams}>Add To Card</button>
+    </div>
+  )
+}
+
+export default Product
+*/
 /*
 import React from 'react'
 
